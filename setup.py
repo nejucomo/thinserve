8c722ba8c13f14d8fed5bb8d77f4ebbcf5717e3c
Nathan Wilcox <nejucomo@gmail.com>
2015-06-06 06:09:52 -0700

    Fix PEP8 failures (from onslaught).

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from setuptools import setup, find_packages
 
 
 PACKAGENAME = 'thinserve'
-INSTALL_REQUIRES = [
-    #'preconditions >= 0.1',
-    #'twisted >= 14.0',
-    #'mock >= 1.0.1',
-    ]
 
 
 setup(
     name=PACKAGENAME,
-    description='A thin server library for RESTful api protocol with a static js+html client.',
+    description="""\
+        A thin server framework for REST API + static js+html clients.',
+    """.strip(),
     url='https://github.com/nejucomo/{0}'.format(PACKAGENAME),
     license='GPLv3',
     version='0.1.dev0',
@@ -21,8 +18,8 @@ setup(
     author_email='nejucomo@gmail.com',
 
     packages=find_packages(),
-    install_requires=INSTALL_REQUIRES,
-    package_data = {
+    install_requires=[],
+    package_data={
         PACKAGENAME: [
             'web/static/*',
         ]
