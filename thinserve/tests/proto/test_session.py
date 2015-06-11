46c474b744b64805537cc7a275741bf9ddbdd233
Nathan Wilcox <nejucomo@gmail.com>
2015-06-10 20:01:22 -0600

    Implement multiple messages checked for two flows: receive calls -> gather replies & gather calls -> receive replies.

--- thinserve/tests/proto/test_session.py
+++ thinserve/tests/proto/test_session.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from mock import MagicMock, call, sentinel
+from mock import MagicMock, call
 from thinserve.proto import session
 from thinserve.tests.testutil import check_mock, check_lists_equal
 
@@ -8,6 +8,16 @@ class SessionTests (TestCase):
     def setUp(self):
         self.m_root = MagicMock(name='createsession')
         self.s = session.Session(self.m_root)
+
+        # Setup some immediate call/reply info:
+        self.replies = []
+        self.params = []
+
+        setupinfo = [('Yum {}!', self.replies),
+                     ('Fruit #{}', self.params)]
+        for i in range(17):
+            for tmpl, container in setupinfo:
+                container.append(tmpl.format(i))
 
     def test_empty_gather_outgoing_messages(self):
         d = self.s.gather_outgoing_messages()
@@ -19,29 +29,82 @@ class SessionTests (TestCase):
         # The deferred should be waiting to fire:
         self.failIf(d.called)
 
-    def test_receive_call_then_gather_one_message(self):
-        self.m_root.eat_a_fruit.return_value = 'Yum!'
-        self.s.receive_message(
-            ['call',
-             {'id': sentinel.callid,
-              'target': None,
-              'method': 'eat_a_fruit',
-              'params': {'fruit': 'apple'}}])
+    def test_receive_n_immediate_calls_then_gather_n_replies(self):
+        self.m_root.eat_a_fruit.side_effect = self.replies
+
+        for callid, param in enumerate(self.params):
+            self.s.receive_message(
+                ['call',
+                 {'id': callid,
+                  'target': None,
+                  'method': 'eat_a_fruit',
+                  'params': {'fruit': param}}])
 
         d = self.s.gather_outgoing_messages()
 
         check_mock(
             self, self.m_root,
-            [call.eat_a_fruit(fruit='apple')])
+            [call.eat_a_fruit(fruit=p) for p in self.params])
 
         # The deferred be called with a reply:
         self.failUnless(d.called)
 
         @d.addCallback
-        def take_messages(replies):
+        def take_messages(messages):
             check_lists_equal(
                 self,
-                [['reply',
-                  {'id': sentinel.callid,
-                   'result': ['data', 'Yum!']}]],
-                replies)
+                [
+                    ['reply',
+                     {'id': callid,
+                      'result': ['data', reply]}]
+
+                    for callid, reply
+                    in enumerate(self.replies)
+                ],
+                messages)
+
+    def test_gather_n_calls_then_receive_n_replies(self):
+        fakeid = 'fake-client-id'
+
+        repdefs = []
+
+        # Prime the (private) outgoing call queue:
+        for callid, param in enumerate(self.params):
+            repdefs.append(
+                self.session._send_call(
+                    target=fakeid,
+                    callid=callid,
+                    method='eat_a_fruit',
+                    params={'fruit': param}))
+
+        d = self.s.gather_outgoing_messages()
+
+        self.failUnless(d.called)
+
+        @d.addCallback
+        def take_messages(messages):
+            check_lists_equal(
+                self,
+                [
+                    ['call',
+                     {'id': callid,
+                      'target': fakeid,
+                      'method': 'eat_a_fruit',
+                      'params': {'fruit': param}}]
+
+                    for callid, reply
+                    in enumerate(self.params)
+                ],
+                messages)
+
+            for callid, (reply, d) in enumerate(zip(self.replies, repdefs)):
+                self.failIf(d.called)
+
+                self.s.receive_message(
+                    ['reply',
+                     {'id': callid,
+                      'result': ['data', reply]}])
+
+                self.failUnless(d.called)
+
+                d.addCallback(lambda res: self.assertIs(res, reply))
