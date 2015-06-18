a86a531dbc52fd427fe36cb35410b971d1d4e66e
Nathan Wilcox <nejucomo@gmail.com>
2015-06-18 08:17:48 -0600

    Add a test to expose a bug in @Referenceable.Method.

--- thinserve/tests/api/test_referenceable.py
+++ thinserve/tests/api/test_referenceable.py
@@ -33,12 +33,19 @@ class ReferenceableTests (TestCase):
 
         self.failIf(Referenceable._check(j))
 
-    def test_pos_get_bound_remote_method(self):
+    def test_pos_methods(self):
 
         @Referenceable
         class C (object):
-            pass
+            @Referenceable.Method
+            def foo(self, x):
+                return (self, x)
 
         i = C()
 
         self.failUnless(Referenceable._check(i))
+        self.assertEqual((i, 42), i.foo(42))
+
+        brm = Referenceable._get_bound_methods(i)
+        self.assertEqual(['foo'], brm.keys())
+        self.assertEqual((i, 17), brm['foo'](x=17))
