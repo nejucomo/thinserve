45c281e673d1024d5922c6abba73ceebcaeedd01
Nathan Wilcox <nejucomo@gmail.com>
2015-06-18 08:21:47 -0600

    Add test coverage for @Referenceable.Method_without_prefix.

--- thinserve/tests/api/test_referenceable.py
+++ thinserve/tests/api/test_referenceable.py
@@ -49,3 +49,20 @@ class ReferenceableTests (TestCase):
         brm = Referenceable._get_bound_methods(i)
         self.assertEqual(['foo'], brm.keys())
         self.assertEqual((i, 17), brm['foo'](x=17))
+
+    def test_pos_method_without_prefix(self):
+
+        @Referenceable
+        class C (object):
+            @Referenceable.Method_without_prefix('_remote_')
+            def _remote_foo(self, x):
+                return (self, x)
+
+        i = C()
+
+        self.failUnless(Referenceable._check(i))
+        self.assertEqual((i, 42), i._remote_foo(42))
+
+        brm = Referenceable._get_bound_methods(i)
+        self.assertEqual(['foo'], brm.keys())
+        self.assertEqual((i, 17), brm['foo'](x=17))
